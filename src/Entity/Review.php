6cbcf1f9764a40fbd31873de340bb3440f6c4309
kin29 <kin29.com@gmail.com>
2022-06-03 23:57:56 +0900

    datetime -> dateに変更

--- src/Entity/Review.php
+++ src/Entity/Review.php
@@ -25,7 +25,7 @@ class Review
     #[ORM\Column(type: 'string', length: 255)]
     private string $author = '';
 
-    #[ORM\Column(type: 'datetime', nullable: true)]
+    #[ORM\Column(type: 'date', nullable: true)]
     private ?\DateTimeImmutable $publicationDate = null;
 
     #[ORM\ManyToOne(targetEntity: Book::class, inversedBy: 'reviews')]
