704b1bdbc2f4fa376b5bb1dfdc5ec52ead4e5670
kin29 <kin29.com@gmail.com>
2022-06-03 23:46:44 +0900

    bin/console make:entity --api-resource Review

---
+++ src/Entity/Review.php
@@ -0,0 +1,83 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ReviewRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReviewRepository::class)]
+#[ApiResource]
+class Review
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id;
+
+    #[ORM\Column(type: 'integer')]
+    private int $rating = 0;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $body = '';
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $author = '';
+
+    #[ORM\Column(type: 'datetime_immutable', nullable: true)]
+    private ?\DateTimeImmutable $publicationDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(int $rating): self
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getPublicationDate(): ?\DateTimeImmutable
+    {
+        return $this->publicationDate;
+    }
+
+    public function setPublicationDate(?\DateTimeImmutable $publicationDate): self
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+}
